123e2d25fb5e4a518f1bdaaf9d8688fe036a093f
Chris Gough <christopher.d.gough@gmail.com>
2019-03-10 22:50:52 +1100

    add mvp views.py
    fixes #6

--- bd_teamsport/views.py
+++ bd_teamsport/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the BD Teamsport index.")
+class IndexView(TemplateView):
+    template_name = "bd_teamsport/index.html"
