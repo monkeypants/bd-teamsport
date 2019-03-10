b8c45f0c313e5de6e00ce924ec21993af24520e5
Chris Gough <christopher.d.gough@gmail.com>
2019-03-10 21:51:49 +1100

    stub models re: #4

--- bd_teamsport/models.py
+++ bd_teamsport/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+
+class Opportunity(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    notes = models.TextField()
+    due_date = models.DateTimeField()
+
+    def status(self):
+        proposals = Proposal.objects.filter(opportunity_id=self.id).all()
+        if len(proposals) == 0:
+            return "no proposal"
+        else:
+            for p in proposals:
+                if p.has_contract():
+                    return "won"
+                # the rest could be smarter
+                # if we submitt one but get feedback and a chance to submit another,
+                # then we create the new one but haven't submitted it yet,
+                # then the status should probably be "WIP" rather than "submitted"
+                if p.is_submitted():
+                    return "submitted"
+            return "WIP"
+
+
+class Proposal(models.Model):
+    opportunity = models.ForeignKey(Opportunity, on_delete=models.PROTECT)
+    commenced = models.DateTimeField()  # default now()
+    submitted = models.DateTimeField()  # default None
+    contract_ref = models.CharField(max_length=250)
+
+    def is_submitted(self):
+        if self.submitted:
+            return True
+        else:
+            return False
+
+    def has_contract(self):
+        if self.contract_ref:
+            return True
+        else:
+            return False
