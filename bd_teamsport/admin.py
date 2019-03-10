d70f250f39ab96388aa84544f52d49c610e68508
Chris Gough <christopher.d.gough@gmail.com>
2019-03-10 22:10:27 +1100

    configure admin.py
    fixes #5

--- bd_teamsport/admin.py
+++ bd_teamsport/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Client,
+    Opportunity,
+    Proposal
+)
+
+admin.site.register(Client)
+admin.site.register(Opportunity)
+admin.site.register(Proposal)
